2c605b474709be4a401ba12019489d2f66d9889a
hexu <hexu_1985@sina.com>
2022-08-04 11:52:37 +0800

    update cxx/tcpcliserv/recipe-01

--- code/cxx/tcpcliserv/recipe-01/tcp_echo_client.cpp
+++ code/cxx/tcpcliserv/recipe-01/tcp_echo_client.cpp
@@ -1,40 +1,32 @@
 #include <boost/asio.hpp>
 #include <iostream>
+#include <string>
+#include "error_handling.hpp"
 
 using namespace boost;
 
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc != 3)
+        error_handling("usage: %s <IPaddress> <Port>", argv[0]);
+
 	// Step 1. Assume that the client application has already
 	// obtained the IP address and protocol port number of the
 	// target server.
-	std::string raw_ip_address = "127.0.0.1";
-	unsigned short port_num = 9999;
+	std::string raw_ip_address = argv[1];
+	unsigned short port_num = std::stoi(argv[2]);
 
-	try {
-		// Step 2. Creating an endpoint designating 
-		// a target server application.
-		asio::ip::tcp::endpoint
-			ep(asio::ip::address::from_string(raw_ip_address),
-			port_num);
+    // Step 2. Creating an endpoint designating 
+    // a target server application.
+    asio::ip::tcp::endpoint ep(asio::ip::address::from_string(raw_ip_address), port_num);
 
-		asio::io_service ios;
+    asio::io_service ios;
 
-		// Step 3. Creating and opening a socket.
-		asio::ip::tcp::socket sock(ios, ep.protocol());
+    // Step 3. Creating and opening a socket.
+    asio::ip::tcp::socket sock(ios, ep.protocol());
 
-		// Step 4. Connecting a socket.
-		sock.connect(ep);
-	}
-	// Overloads of asio::ip::address::from_string() and 
-	// asio::ip::tcp::socket::connect() used here throw
-	// exceptions in case of error condition.
-	catch (system::system_error &e) {
-		std::cout << "Error occured! Error code = " << e.code()
-			<< ". Message: " << e.what();
-
-		return e.code().value();
-	}
+    // Step 4. Connecting a socket.
+    sock.connect(ep);
 
 	return 0;
 }
