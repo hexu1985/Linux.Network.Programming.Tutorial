1186d7e8717e1f9c990ab5bec1c0dad9a36974ba
hexu <hexu_1985@sina.com>
2022-09-26 15:55:57 +0800

    add tcp/tcp_sixteen/cxx

---
+++ tcp/tcp_sixteen/cxx/error.cpp
@@ -0,0 +1,18 @@
+#include "error.hpp"
+
+#include <stdarg.h>
+#include <sstream>
+
+std::string error_message(const char* file, unsigned int line, const char* format, ...)
+{
+    const int buf_size = 1024;
+    char buf[buf_size] = {'\0'};
+    va_list ap;
+
+    va_start (ap, format);
+    vsnprintf(buf, buf_size, format, ap);
+    va_end (ap);
+    std::ostringstream os;
+    os << "[" << file << ":" << line << "] " << buf;
+    return os.str();
+}
---
+++ tcp/tcp_sixteen/cxx/error.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <errno.h>
+#include <string>
+#include <stdexcept>
+#include <system_error>
+
+std::string error_message(const char* file, int line, const char* format, ...);
+
+#define ThrowRuntimeError(format, ...) \\
+    throw std::runtime_error(error_message(__FILE__, __LINE__, format, __VA_ARGS__))
+
+#define ThrowSystemError(format, ...) \\
+    throw std::system_error(errno, std::system_category(), \\
+            error_message(__FILE__, __LINE__, format, __VA_ARGS__))
+
